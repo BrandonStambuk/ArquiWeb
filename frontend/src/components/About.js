95821432de4a968a597ba6b56501ab0fb25d2e24
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2023-11-17 13:11:42 -0400

    apartado about

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,23 +1,36 @@
+// Importa la clase de estilo
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/ImageSection.css';
+import imageMartinCardenas from '../images/martinCardenas.jpg'; // Importa la imagen
 
 function About() {
   return (
     <div className="container-imagen-aloe">
       <div className="row justify-content-center align-items-center vh-100 imagen-aloe">
-        <div className="col-md-6">
-          <div className="card text-center">
+        <div className="col-md-8"> {/* Aumenta el ancho del card a 8 columnas en pantallas grandes */}
+          <div className="card text-center b-0">
             <div className="card-body">
-              <h5 className="card-title">Título de la tarjeta</h5>
-              <p className="card-text">Contenido de la tarjeta.</p>
+              <div className="row">
+                <div className="col-md-6"> {/* Ocupa 6 columnas en pantallas grandes */}
+                  <h5 className="card-title">Sobre Martin Cardenas</h5>
+                  {/* Aplica ambas clases de estilo al elemento p */}
+                  <p className="card-text custom-card-text justified-text">
+                    Martín Cárdenas es considerado como el botánico más importante que ha tenido Bolivia. Realizó extensas expediciones botánicas por todo el territorio nacional recolectando especímenes de la flora local, de la cual llegó
+                    a reunir 6500 ejemplares. Su prestigio traspasó las fronteras. La mayor parte de sus exploraciones las
+                    realizó con medios propios demostrando que con voluntad y decisión se puede investigar en Bolivia, pese a no tener las condiciones favorables.
+                        <br></br>
+                        
+                  </p>
+                </div>
+                <div className="col-md-6"> {/* Ocupa 6 columnas en pantallas grandes */}
+                  <div className="row">
+                    {/* Utiliza la variable imageMartinCardenas para establecer la ruta de la imagen */}
+                    <img src={imageMartinCardenas} className="card-img-top rounded" alt="Imagen Martin Cardenas" />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card">
-            {/* Agrega tu imagen dentro del cuerpo de la tarjeta */}
-            <img src="ruta_de_la_imagen.jpg" className="card-img-top" alt="Imagen Aloe" />
           </div>
         </div>
       </div>
